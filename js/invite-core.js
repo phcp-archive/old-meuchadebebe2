cbb72fa390bc05c7ade280a0d2f0d65df4502a36
Paulo Henrique da Cruz Pereira <phcp@cin.ufpe.br>
2014-07-27 22:47:49 -0300

    Convite

--- js/invite-core.js
+++ js/invite-core.js
@@ -43,8 +43,8 @@ $(function() {
   // Tela principal do convite
   var InviteView = Parse.View.extend({
     events: {
-      "submit form.invite-form": "aceitar",
-      "submit form.invite-form2": "rejeitar"
+      "click #btn-sim": "aceitar",
+      "click #btn-nao": "rejeitar"
     },
 
     inviteTemplate: _.template($("#invite-template").html()),
@@ -59,15 +59,30 @@ $(function() {
       var query = new Parse.Query(Convite);
       query.equalTo("req_facebook", reqId);
       query.find({
-        success: function(results) {
-          console.log(results);
-          console.log(Parse.User.current());
+        success: function(cvResults) {
+          var innerSelf = self;
 
-          if(!results || results.length == 0) {
+          if(!cvResults || cvResults.length == 0) {
             this.$("#error").html("Você não tem convites aguardando resposta.").show();
           }
           else {
-            var meuConvite = results[0].attributes;
+            var convite = cvResults[0].attributes;
+            var innerQuery = new Parse.Query(Evento);
+            innerQuery.equalTo("user", convite.user);
+            innerQuery.find({
+              success: function(evResults) {
+                if(!evResults || evResults.length == 0) {
+                  this.$("#error").html("Você não tem convites aguardando resposta.").show();
+                }
+                else {
+                  var evento = evResults[0].attributes;
+                  innerSelf.render(evento, convite.mensagem);
+                }
+              },
+              error: function(error) {
+                this.$("#error").html("Problemas ao requisitar dados do servidor, aguarde e tente novamente.").show();
+              }
+            });
           }
         },
         error: function(error) {
@@ -77,31 +92,79 @@ $(function() {
     },
 
     aceitar: function() {
-      this.render();
+      var self = this;
+      var reqId = $.getUrlVar("request_ids").toString();
+
+      var query = new Parse.Query(Convite);
+      query.equalTo("req_facebook", reqId);
+      query.find({
+        success: function(cvResults) {
+          var innerSelf = self;
+          var convite = cvResults[0];
+
+          convite.set("aceitos", convite.attributes.aceitos + 1);
+          convite.save(null, {
+            success: function(conviteResp) {
+              new GiftsListView();
+            },
+            error: function(error) {
+              console.log("error");
+              innerSelf.$("#error").html("Problemas ao salvar dados no servidor, aguarde e tente novamente.").show();
+            }
+          });
+        },
+        error: function(error) {
+          this.$("#error").html("Problemas ao requisitar dados do servidor, aguarde e tente novamente.").show();
+        }
+      });
     },
 
     rejeitar: function() {
-      this.render();
+      new RejectView();
+      this.undelegateEvents();
     },
 
-    render: function(evento) {
-      var dataEvento = new Date(meuEvento.data);
+    render: function(evento, msg) {
+      var dataEvento = new Date(evento.data);
       var dataAgora = new Date($.now());
       var diasCont = Math.floor((dataEvento - dataAgora) / (1000*60*60*24));
 
-      var dataStr = $.format.date(meuEvento.data, "dd/MM/yyyy") + " às " + $.format.date(meuEvento.data, "hh:mm");;
+      var dataStr = $.format.date(evento.data, "dd/MM/yyyy") + " às " + $.format.date(evento.data, "hh:mm");;
 
-      this.$el.html(this.momsTemplate({
-        nomedobebe: meuEvento.nomedobebe,
-        descricao: meuEvento.descricao,
+      this.$el.html(this.inviteTemplate({
+        nomedobebe: evento.nomedobebe,
+        descricao: evento.descricao,
         data: dataStr,
         diasCont: diasCont,
-        convidadosCont: 15,
-        presentesCont: 20
+        mensagem: msg
       }));
-
-      this.$el.html(this.inviteTemplate);
       this.delegateEvents();
+    }
+  });
+
+  // Tela de rejeicao convite
+  var RejectView = Parse.View.extend({
+    el: "#content",
+
+    initialize: function() {
+      this.render();
+    },
+
+    render: function() {
+      this.$el.html(_.template($("#reject-template").html()));
+    }
+  });
+
+  // Tela de presentes do convite
+  var GiftsListView = Parse.View.extend({
+    el: "#content",
+
+    initialize: function() {
+      this.render();
+    },
+
+    render: function() {
+      this.$el.html(_.template($("#giftslist-template").html()));
     }
   });
 
